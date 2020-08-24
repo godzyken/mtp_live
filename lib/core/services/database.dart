87a8e436a545192046e82bd5245e071bb1b7ebd7
godzy <isgodzy@msn.com>
2020-08-24 23:41:02 +0200

    make new the authentication service - en cour

--- lib/core/services/database.dart
+++ lib/core/services/database.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase/firebase.dart';
 import 'package:firebase_database/firebase_database.dart' as db;
 import 'package:googleapis/firestore/v1.dart';
@@ -6,36 +7,18 @@ import '../models/post.dart';
 
 final databaseReference = db.FirebaseDatabase.instance.reference();
 
-//DatabaseReference savePost(Post post) {
-//  var id = databaseReference.child('posts/').push();
-//  id.set(post.toJson());
-//  return id;
-//}
-//
-//void updatePost(Post post, DatabaseReference id) {
-//  id.update(post.toJson());
-//}
-//
-//Future<List<Post>> getAllPosts() async {
-//  DataSnapshot dataSnapshot = await databaseReference.child('posts/').once();
-//  List<Post> posts = [];
-//  if (dataSnapshot.value != null) {
-//    dataSnapshot.value.forEach((key, value) {
-//      Post post = createPost(value);
-//      post.setId(databaseReference.child('posts/' + key));
-//      posts.add(post);
-//    });
-//  }
-//  return posts;
-//}
 final firestore = FirestoreApi.DatastoreScope;
 
 class DatabaseService {
   Future<User> getUser(String id) async {
     db.DataSnapshot snap = await databaseReference.child('user/').once();
     if(snap != null) {
-      return snap.value(id);
+      CollectionReference user = FirebaseFirestore.instance.collection('users');
+      return snap.value(user.id);
     }
+
+
+
   }
 
   db.DatabaseReference savePost(Post post) {
